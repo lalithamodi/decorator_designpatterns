923c3f2d37564cc9bfe6804295f5f7cdf2a4b41a
Lalitha Sagari Modi <167764565+lalithamodi@users.noreply.github.com>
2024-05-09 14:44:44 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type IPizza interface {
+	getPrice() int
+}
+
+// component
+type VeggieMania struct {
+}
+
+func (p *VeggieMania) getPrice() int {
+	return 15
+}
+
+// decorator
+type TomatoTopping struct {
+	pizza IPizza
+}
+
+func (c *TomatoTopping) getPrice() int {
+	pizzaPrice := c.pizza.getPrice()
+	return pizzaPrice + 7
+}
+
+type CheeseTopping struct {
+	pizza IPizza
+}
+
+func (c *CheeseTopping) getPrice() int {
+	pizzaPrice := c.pizza.getPrice()
+	return pizzaPrice + 10
+}
+
+func main() {
+
+	pizza := &VeggieMania{}
+
+	//Add cheese topping
+	pizzaWithCheese := &CheeseTopping{
+		pizza: pizza,
+	}
+
+	//Add tomato topping
+	pizzaWithCheeseAndTomato := &TomatoTopping{
+		pizza: pizzaWithCheese,
+	}
+
+	fmt.Printf("Price of veggeMania with tomato and cheese topping is %d\n", pizzaWithCheeseAndTomato.getPrice())
+}
